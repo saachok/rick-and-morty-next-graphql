3601b4531f766a93aec89ce7913f3e064023d43f
Andrii Sachok <andriy.sachok@gmail.com>
2023-09-04 13:52:23 +0300

    updated layout of home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+import styles from '../public/styles/Home.module.scss';
+
 export default function Home() {
   return (
-    <main>
-      <h1>That&apos;s Home page!</h1>
+    <main className={styles.container}>
+      <h1 className={styles.title}>Rick and Morty</h1>
+      <h2 className={styles.subtitle}>
+        This App was created using Next.js and GraphQL
+      </h2>
+      <p className={styles.description}>
+        Let&apos;s read some info about Rick and Morty Universe
+      </p>
+      <div className={styles.links}>
+        <Link className={styles.characters} href={'/characters'}>
+          Characters
+        </Link>
+        <Link className={styles.episodes} href={'/episodes'}>
+          Episodes
+        </Link>
+        <Link className={styles.locations} href={'/locations'}>
+          Locations
+        </Link>
+      </div>
     </main>
   );
 }
