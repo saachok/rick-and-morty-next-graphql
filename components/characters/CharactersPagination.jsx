0146c79149b4e15546cc0d8db8e8b8ddb6044922
Andrii Sachok <andriy.sachok@gmail.com>
2023-09-09 20:12:48 +0300

    solved merge conflict

--- components/characters/CharactersPagination.jsx
+++ components/characters/CharactersPagination.jsx
@@ -4,36 +4,17 @@ import PaginationButton from '../UI/PaginationButton';
 import StaticPagination from './pagination/StaticPagination';
 import DynamicPagination from './pagination/DynamicPagination';
 
-<<<<<<< HEAD
-const CharactersPagination = ({ pageNumber, setPageNumber }) => {
-=======
 const CharactersPagination = ({ pageNumber, goToPage }) => {
->>>>>>> feat/characters-pagination
   return (
     <div className={styles.container}>
       <div className={styles.nav}>
         <PaginationButton
           disabled={pageNumber > 1 ? false : true}
-<<<<<<< HEAD
-          onClick={() => setPageNumber(prevState => prevState - 1)}
-=======
           onClick={() => goToPage(pageNumber - 1)}
->>>>>>> feat/characters-pagination
         >
           &#60;
         </PaginationButton>
         {pageNumber < 6 ? (
-<<<<<<< HEAD
-          <StaticPagination {...{ pageNumber, setPageNumber }} />
-        ) : pageNumber < 38 ? (
-          <DynamicPagination {...{ pageNumber, setPageNumber }} />
-        ) : (
-          <StaticPagination {...{ pageNumber, setPageNumber }} />
-        )}
-        <PaginationButton
-          disabled={pageNumber >= 42 ? true : false}
-          onClick={() => setPageNumber(prevState => prevState + 1)}
-=======
           <StaticPagination {...{ pageNumber, goToPage }} />
         ) : pageNumber < 38 ? (
           <DynamicPagination {...{ pageNumber, goToPage }} />
@@ -43,7 +24,6 @@ const CharactersPagination = ({ pageNumber, goToPage }) => {
         <PaginationButton
           disabled={pageNumber >= 42 ? true : false}
           onClick={() => goToPage(pageNumber + 1)}
->>>>>>> feat/characters-pagination
         >
           &#62;
         </PaginationButton>
--- components/characters/pagination/DynamicPagination.jsx
+++ components/characters/pagination/DynamicPagination.jsx
@@ -1,43 +1,5 @@
 import PaginationButton from '@/components/UI/PaginationButton';
 
-<<<<<<< HEAD
-const DynamicPagination = ({ pageNumber, setPageNumber }) => {
-  return (
-    <>
-      <PaginationButton onClick={() => setPageNumber(1)}>1</PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState - 3)}
-      >
-        ...
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState - 2)}
-      >
-        {pageNumber - 2}
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState - 1)}
-      >
-        {pageNumber - 1}
-      </PaginationButton>
-      <PaginationButton isActive={true}>{pageNumber}</PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState + 1)}
-      >
-        {pageNumber + 1}
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState + 2)}
-      >
-        {pageNumber + 2}
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => setPageNumber(prevState => prevState + 3)}
-      >
-        ...
-      </PaginationButton>
-      <PaginationButton onClick={() => setPageNumber(42)}>42</PaginationButton>
-=======
 const DynamicPagination = ({ pageNumber, goToPage }) => {
   return (
     <>
@@ -62,7 +24,6 @@ const DynamicPagination = ({ pageNumber, goToPage }) => {
         ...
       </PaginationButton>
       <PaginationButton onClick={() => goToPage(42)}>42</PaginationButton>
->>>>>>> feat/characters-pagination
     </>
   );
 };
