6b6bb6e8da400193481b5b586b3852ac4c12deb8
Andrii Sachok <andriy.sachok@gmail.com>
2023-09-09 20:08:38 +0300

    Changed request logic
    Read pageNubmer value from query params instead of state

--- components/characters/CharactersPagination.jsx
+++ components/characters/CharactersPagination.jsx
@@ -4,17 +4,26 @@ import PaginationButton from '../UI/PaginationButton';
 import StaticPagination from './pagination/StaticPagination';
 import DynamicPagination from './pagination/DynamicPagination';
 
+<<<<<<< HEAD
 const CharactersPagination = ({ pageNumber, setPageNumber }) => {
+=======
+const CharactersPagination = ({ pageNumber, goToPage }) => {
+>>>>>>> feat/characters-pagination
   return (
     <div className={styles.container}>
       <div className={styles.nav}>
         <PaginationButton
           disabled={pageNumber > 1 ? false : true}
+<<<<<<< HEAD
           onClick={() => setPageNumber(prevState => prevState - 1)}
+=======
+          onClick={() => goToPage(pageNumber - 1)}
+>>>>>>> feat/characters-pagination
         >
           &#60;
         </PaginationButton>
         {pageNumber < 6 ? (
+<<<<<<< HEAD
           <StaticPagination {...{ pageNumber, setPageNumber }} />
         ) : pageNumber < 38 ? (
           <DynamicPagination {...{ pageNumber, setPageNumber }} />
@@ -24,6 +33,17 @@ const CharactersPagination = ({ pageNumber, setPageNumber }) => {
         <PaginationButton
           disabled={pageNumber >= 42 ? true : false}
           onClick={() => setPageNumber(prevState => prevState + 1)}
+=======
+          <StaticPagination {...{ pageNumber, goToPage }} />
+        ) : pageNumber < 38 ? (
+          <DynamicPagination {...{ pageNumber, goToPage }} />
+        ) : (
+          <StaticPagination {...{ pageNumber, goToPage }} />
+        )}
+        <PaginationButton
+          disabled={pageNumber >= 42 ? true : false}
+          onClick={() => goToPage(pageNumber + 1)}
+>>>>>>> feat/characters-pagination
         >
           &#62;
         </PaginationButton>
