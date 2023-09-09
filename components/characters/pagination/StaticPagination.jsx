7bf08d54f2a36fb628ca8fff4d3edb58ecd9326b
Andrii Sachok <andriy.sachok@gmail.com>
2023-09-09 20:12:48 +0300

    solved merge conflict

--- components/characters/pagination/StaticPagination.jsx
+++ components/characters/pagination/StaticPagination.jsx
@@ -1,21 +1,13 @@
 import PaginationButton from '@/components/UI/PaginationButton';
 
-<<<<<<< HEAD
-const StaticPagination = ({ pageNumber, setPageNumber }) => {
-=======
 const StaticPagination = ({ pageNumber, goToPage }) => {
->>>>>>> feat/characters-pagination
   const FirstPages = () => {
     const firstButtons = [];
     for (let i = 1; i <= 7; i++) {
       firstButtons.push(
         <PaginationButton
           key={i}
-<<<<<<< HEAD
-          onClick={() => setPageNumber(i)}
-=======
           onClick={() => goToPage(i)}
->>>>>>> feat/characters-pagination
           isActive={pageNumber === i ? true : false}
         >
           {i}
@@ -25,17 +17,8 @@ const StaticPagination = ({ pageNumber, goToPage }) => {
     return (
       <>
         {firstButtons.map(btn => btn)}
-<<<<<<< HEAD
-        <PaginationButton onClick={() => setPageNumber(8)}>
-          ...
-        </PaginationButton>
-        <PaginationButton onClick={() => setPageNumber(42)}>
-          42
-        </PaginationButton>
-=======
         <PaginationButton onClick={() => goToPage(8)}>...</PaginationButton>
         <PaginationButton onClick={() => goToPage(42)}>42</PaginationButton>
->>>>>>> feat/characters-pagination
       </>
     );
   };
@@ -46,11 +29,7 @@ const StaticPagination = ({ pageNumber, goToPage }) => {
       lastButtons.push(
         <PaginationButton
           key={i}
-<<<<<<< HEAD
-          onClick={() => setPageNumber(i)}
-=======
           onClick={() => goToPage(i)}
->>>>>>> feat/characters-pagination
           isActive={pageNumber === i ? true : false}
         >
           {i}
@@ -59,15 +38,8 @@ const StaticPagination = ({ pageNumber, goToPage }) => {
     }
     return (
       <>
-<<<<<<< HEAD
-        <PaginationButton onClick={() => setPageNumber(1)}>1</PaginationButton>
-        <PaginationButton onClick={() => setPageNumber(35)}>
-          ...
-        </PaginationButton>
-=======
         <PaginationButton onClick={() => goToPage(1)}>1</PaginationButton>
         <PaginationButton onClick={() => goToPage(35)}>...</PaginationButton>
->>>>>>> feat/characters-pagination
         {lastButtons.map(btn => btn)}
       </>
     );
