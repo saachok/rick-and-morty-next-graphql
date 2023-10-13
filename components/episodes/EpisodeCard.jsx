890acb52765571e97c49ac28a07830d37515bced
Andrii Sachok <andriy.sachok@gmail.com>
2023-10-13 18:55:04 +0300

    Changed episode card styles
    commit a13ad4befa09ab7ed6851fbe17fad17324a0f81f
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Fri Oct 13 18:54:28 2023 +0300
    
        Updated episode card styles

--- components/episodes/EpisodeCard.jsx
+++ components/episodes/EpisodeCard.jsx
@@ -5,7 +5,7 @@ const EpisodeCard = ({ name, air_date, href }) => {
   return (
     <Link href={href} className={styles.link}>
       <div className={styles.container}>
-        <h1 className={styles.name}>{name}</h1>
+        <h2 className={styles.name}>{name}</h2>
         <p className={styles['air-date']}>{air_date}</p>
       </div>
     </Link>
